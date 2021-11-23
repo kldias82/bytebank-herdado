75f594926be711e383971207a976b092f32ef157
Kleber Dias <kldias@gmail.com>
2021-11-23 09:40:02 -0300

    Upload code

---
+++ bytebank-herdado/src/EditorVideo.java
@@ -0,0 +1,9 @@
+
+public class EditorVideo extends Funcionario {
+	
+	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificacao do Editor de Vídeo");
+		return 150;
+	}
+
+}
